c40e99d2e577c2048fffe84b6120a5e0ccad6e4c
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-30 00:36:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ function initForm() {
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
-
+function createRecipe() {
+  var recipe = getRecipeVals()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
 
 function handlebarsSetup() {
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
