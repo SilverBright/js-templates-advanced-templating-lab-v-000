a93a44c0ba4727d8cc9afb8a92e3902b2c1dd524
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-30 00:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ function initForm() {
 
 
 
-
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
 
 function init() {
   //put any page initialization/handlebars initialization here
