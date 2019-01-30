7db5c65c9d6e3359a1763eff3c8ce31dba99077e
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-30 00:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ function displayEditForm() {
   for(var i=0;i<ingredientsNodes.length;i++) {
     ingredients.push(ingredientsNodes[i].innerText)
   }
+  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(recipeFormTemplate)
+  
+  document.getElementById("main").innerHTML = template(recipe)
 }
 
 
